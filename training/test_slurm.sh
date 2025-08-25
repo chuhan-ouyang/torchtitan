1ab4e35b31705c5beb8fa3394441cbf65777541c
chuhan <chuhan.ouyang@gmail.com>
2025-08-25 13:21:33 -0700

    enable 1 node slurm: test_slurm.sh for debug model

--- training/test_slurm.sh
+++ training/test_slurm.sh
@@ -11,6 +11,13 @@ head_node=${nodes_array[0]}
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 echo Node IP: $head_node_ip
 
+# Custom NCCL path for NCCL 2.27 build
+# export NCCL_HOME=/global/u2/c/co232/opus-wksp/nccl/build
+# export LD_LIBRARY_PATH="$NCCL_HOME/lib:$(echo "$LD_LIBRARY_PATH" | tr ':' '\n' | grep -v 'nccl' | paste -sd: -)"
+# export LD_PRELOAD=$NCCL_HOME/lib/libnccl.so.2.27.6
+# export LIBRARY_PATH="$NCCL_HOME/lib:$LIBRARY_PATH"
+# export CPATH="$NCCL_HOME/include:$CPATH"
+
 # From multinode_trainer.slurm
 export LOGLEVEL=INFO
 # Enable for A100
@@ -19,12 +26,16 @@ export PYTHONFAULTHANDLER=1
 export CUDA_LAUNCH_BLOCKING=0
 
 export NCCL_DEBUG=INFO
-export NCCL_DEBUG_SUBSYS=INIT,GRAPH,NET
+export NCCL_DEBUG_SUBSYS=INIT,NET,COLL
+
+export NCCL_PROTO=Simple
+export NCCL_ALGO=Ring
+
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 export NCCL_BUFFSIZE=1048576
 
-CONFIG_FILE=${CONFIG_FILE:-"./torchtitan/models/deepseek_v3/train_configs/deepseek_v3_16b.toml"}
+CONFIG_FILE=${CONFIG_FILE:-"./torchtitan/models/deepseek_v3/train_configs/debug_model.toml"}
 
 # From run_train.sh
 # TORCHFT_LIGHTHOUSE=${TORCHFT_LIGHTHOUSE:-"http://localhost:29510"}
@@ -36,13 +47,13 @@ dcgmi profile --pause
 srun \
   --job-name=torchtitan_multi_node \
   --partition=train \
-  --nodes=4 \
+  --nodes=1 \
   --ntasks-per-node=1 \
   --gpus-per-task=4 \
   --cpus-per-task=16 \
   --kill-on-bad-exit=1 \
   --export=ALL \
-  torchrun --nnodes 4 --nproc_per_node 4 --rdzv_id tt_multi_${SLURM_JOB_ID} --rdzv_backend c10d --rdzv_endpoint "$head_node_ip:29500"\
+  torchrun --nnodes 1 --nproc_per_node 4 \
   -m torchtitan.train --job.config_file ${CONFIG_FILE} "$@" \
 
 dcgmi profile --resume
